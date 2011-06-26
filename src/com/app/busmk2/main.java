38bd4530a224c124b95f8a68152315f759040a7b
Bojan Ilievski <drakuwa@gmail.com>
2011-06-26 02:58:59 +0200

    initial commit

---
+++ src/com/app/busmk2/main.java
@@ -0,0 +1,14 @@
+package com.app.busmk2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
---
+++ src/com/app/busmk2/splash.java
@@ -0,0 +1,27 @@
+package com.app.busmk2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends Activity {
+	
+	protected boolean _active = true;
+	protected int _splashTime = 2300;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        
+        new Handler().postDelayed(new Runnable(){
+        	public void run() {
+        	Intent novIntent = new Intent(splash.this,main.class);
+        	startActivity(novIntent);
+        	finish();
+
+        	}},2300);    
+    }
+    
+}
