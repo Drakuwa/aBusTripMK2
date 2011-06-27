a714230e28b720190f01d2d1dff3358543f13ac8
Bojan Ilievski <drakuwa@gmail.com>
2011-06-27 03:42:51 +0200

    Changes in TODO

---
+++ src/com/app/busmk2/More.java
@@ -0,0 +1,45 @@
+package com.app.busmk2;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class More extends TabActivity{
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_tab);
+        
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, temp.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("about").setIndicator("About",
+                res.getDrawable(R.drawable.ic_tab_about))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, temp.class);
+        spec = tabHost.newTabSpec("help").setIndicator("Help",
+                res.getDrawable(R.drawable.ic_tab_help))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, temp.class);
+        spec = tabHost.newTabSpec("contact").setIndicator("Contact",
+                res.getDrawable(R.drawable.ic_tab_contact))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(1);        
+    }
+
+}
