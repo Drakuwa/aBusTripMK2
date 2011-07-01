06617cabc2e21e98e278202a38611e1df7c34591
Bojan Ilievski <drakuwa@gmail.com>
2011-07-01 17:27:09 +0200

    added MyItemizedOverlay class without pathoverlay ...

---
+++ src/com/app/busmk2/MyItemizedOverlay.java
@@ -0,0 +1,56 @@
+package com.app.busmk2;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MyItemizedOverlay extends ItemizedOverlay {
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+
+	public MyItemizedOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+	
+	public void addOverlay(ArrayList<OverlayItem> items) {
+		mOverlays.addAll(items);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+	public MyItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
