26ac525e6916fd07bf83699d71215f4b62b4facd
Bojan Ilievski <drakuwa@gmail.com>
2011-07-02 05:45:46 +0200

    simplified the webview pics with added pinch to zoom support (all exept 65b)

--- src/com/app/busmk2/a15.java
+++ src/com/app/busmk2/a15.java
@@ -2,20 +2,11 @@ package com.app.busmk2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.FrameLayout;
 
 public class a15 extends Activity {
     
 	private WebView myWebView;
-	private static final FrameLayout.LayoutParams ZOOM_PARAMS =
-	new FrameLayout.LayoutParams(
-	ViewGroup.LayoutParams.FILL_PARENT,
-	ViewGroup.LayoutParams.WRAP_CONTENT,
-	Gravity.BOTTOM);
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,13 +16,9 @@ public class a15 extends Activity {
         this.setContentView(R.layout.line_pic);
         this.myWebView = (WebView) this.findViewById(R.id.line_pic);
         
-        FrameLayout mContentView = (FrameLayout) getWindow().
-        getDecorView().findViewById(android.R.id.content);
-        final View zoom = this.myWebView.getZoomControls();
-        mContentView.addView(zoom, ZOOM_PARAMS);
-        zoom.setVisibility(View.GONE);
+        myWebView.getSettings().setSupportZoom(true);
+        myWebView.getSettings().setBuiltInZoomControls(true);
 
         this.myWebView.loadUrl("file:///android_asset/a15.jpg");
-        //this.myWebView.setBackgroundResource(R.drawable.a15);
     }
 }
