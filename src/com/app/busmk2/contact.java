d51a4768b44ff98ca3c1a6772098fc3633b0dc17
Bojan Ilievski <drakuwa@gmail.com>
2011-07-03 23:49:56 +0200

    added android version controlled emailing ...

--- src/com/app/busmk2/contact.java
+++ src/com/app/busmk2/contact.java
@@ -3,10 +3,12 @@ package com.app.busmk2;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class contact extends Activity {
 
@@ -14,7 +16,7 @@ public class contact extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contact);
-		
+
 		ImageView twitter = (ImageView) findViewById(R.id.twitter);
 		twitter.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
@@ -23,7 +25,7 @@ public class contact extends Activity {
 				startActivity(myIntent);
 			}
 		});
-		
+
 		ImageView facebook = (ImageView) findViewById(R.id.facebook);
 		facebook.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
@@ -32,15 +34,25 @@ public class contact extends Activity {
 				startActivity(myIntent);
 			}
 		});
-		
+
 		ImageView email = (ImageView) findViewById(R.id.email);
 		email.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
-				Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri
-						.parse("mailto:drakuwa@gmail.com"));
-				startActivity(myIntent);
+				if (!(Integer.parseInt(Build.VERSION.SDK) < 8)) {
+					Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri
+							.parse("mailto:drakuwa@gmail.com"));
+					startActivity(myIntent);
+				} else
+					Toast
+							.makeText(
+									getApplicationContext(),
+									"Android верзија "
+											+ Build.VERSION.RELEASE
+											+ " не подржува директно повикување "
+											+ "на Email клиент. Испратете mail на drakuwa@gmail.com",
+									Toast.LENGTH_LONG).show();
 			}
 		});
-		
+
 	}
 }
