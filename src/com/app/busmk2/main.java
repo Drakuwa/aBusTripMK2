f00eaa16ee254cace743a5e684ed27f728c0d222
Bojan Ilievski <drakuwa@gmail.com>
2011-07-03 17:59:07 +0200

    added welcome note for first run of application...

--- src/com/app/busmk2/main.java
+++ src/com/app/busmk2/main.java
@@ -1,5 +1,8 @@
 package com.app.busmk2;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -17,6 +20,31 @@ public class main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		// First run
+		boolean exists = (new File("/data/data/com.app.busmk2/notwelcomefirst"))
+				.exists();
+
+		if (!exists) {
+			//Welcome note...
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage("Добредојдовте во aBusTripMK2, " +
+					"за повеќе информации и начин на користење, " +
+					"погледнете во 'More...' делот ")
+				.setIcon(R.drawable.icon).setTitle(
+					R.string.app_name).setCancelable(false).setPositiveButton(
+					"OK..", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+						}
+					});
+			AlertDialog alert = builder.create();
+			alert.show();
+			try {
+				new File("/data/data/com.app.busmk2/notwelcomefirst").createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		ImageView mapa = (ImageView) findViewById(R.id.map);
 		mapa.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
