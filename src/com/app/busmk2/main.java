1f2f30d6f4a694c059fd3b2aa08cd7c0044c8902
Bojan Ilievski <drakuwa@gmail.com>
2011-07-04 18:52:07 +0200

    added comments to the main class

--- src/com/app/busmk2/main.java
+++ src/com/app/busmk2/main.java
@@ -10,18 +10,35 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
+/**
+ * The main dashboard class which connects the ImageView objects with their
+ * functionalities.
+ * 
+ * @author drakuwa
+ */
 public class main extends Activity {
-	
+
+	// Initialization of the model
 	Model model = new Model(this);
 
+	/**
+	 * An override of the onCreate method that initializes the ImageView objects
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// First run
+		/**
+		 * Calls the model First run method to check if the application is ran
+		 * for the first time
+		 */
 		model.first_run();
-		
+
+		/**
+		 * The next 3 ImageView variables start new activities with calling a
+		 * new intent of the given classes.
+		 */
 		ImageView mapa = (ImageView) findViewById(R.id.map);
 		mapa.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -49,6 +66,10 @@ public class main extends Activity {
 			}
 		});
 
+		/**
+		 * An ImageView variable that redirects the application to an Internet
+		 * location given with its URL.
+		 */
 		ImageView raspored = (ImageView) findViewById(R.id.raspored);
 		raspored.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
@@ -58,6 +79,9 @@ public class main extends Activity {
 			}
 		});
 
+		/**
+		 * An ImageView variable that calls the exit_dialog() on click.
+		 */
 		ImageView logo_exit = (ImageView) findViewById(R.id.upper);
 		logo_exit.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
@@ -65,6 +89,9 @@ public class main extends Activity {
 			}
 		});
 
+		/**
+		 * An ImageView variable that starts the More activity on click
+		 */
 		ImageView more = (ImageView) findViewById(R.id.more);
 		more.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
@@ -75,16 +102,21 @@ public class main extends Activity {
 		});
 	}
 
+	/**
+	 * Private method that creates and shows an exit dialog window, and exits the
+	 * application on positive answer.
+	 */
 	private void exit_dialog() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage("Are you sure you want to exit?").setIcon(
-				R.drawable.icon).setTitle(R.string.app_name).setCancelable(
-				false).setPositiveButton("OK",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						finish();
-					}
-				});
+		builder.setMessage(
+				"Дали сте сигурни дека саката да ја напуштите апликацијата?")
+				.setIcon(R.drawable.icon).setTitle(R.string.app_name)
+				.setCancelable(false).setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								finish();
+							}
+						});
 		builder.setNegativeButton("Cancel",
 				new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
